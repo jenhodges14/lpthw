103211ebbf81ef147bde6374a916b7ce59c6e763
Jen Hodges <jenhodges14@gmail.com>
2014-12-17 22:23:46 +0000

    make while-loop a function with two arguments - ex33

--- ex33.py
+++ ex33.py
@@ -1,16 +1,23 @@
+# create variables
 i = 0
 numbers = []
 
-while i < 6:
-  print "At the top i is %d" % i
-  numbers.append(i)
+# define functions
+def testing(number, increment):
+  global i
+  while i < number:
+    print "At the top i is %d" % i
+    numbers.append(i)
   
-  i += 1
-  print "Numbers now: ", numbers
-  print "At the bottom i is %d" % i
+    i += increment
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
   
   
-  print "The numbers: "
+    print "The numbers: "
   
-  for num in numbers:
-    print num
+    for num in numbers:
+      print num
+     
+# begin the test    
+testing(6, 2)
