a0e6b51579693e7594941b40b764ad7eea19cb03
Jen Hodges <jenhodges14@gmail.com>
2014-12-17 22:36:09 +0000

    added for loop ex33

--- ex33.py
+++ ex33.py
@@ -3,6 +3,7 @@ i = 0
 numbers = []
 
 # define functions
+# define while loop function
 def testing(number, increment):
   global i
   while i < number:
@@ -18,6 +19,20 @@ def testing(number, increment):
   
     for num in numbers:
       print num
+      
+# define for loop and range function
+def testing_for_loop():
+  numbers = []
+  for i in range(0, 6):
+    numbers.append(i)
+    
      
-# begin the test    
+# begin the test  
+
+# run with while loop function
 testing(6, 2)
+print numbers
+
+# run with for loop and range function
+testing_for_loop()
+print numbers
